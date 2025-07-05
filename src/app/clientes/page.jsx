1ee39ab411796777d036b3ea94ceb724eccacf6b
danielvalmeida91 <danielvalmeida91@gmail.com>
2025-07-04 22:09:14 -0300

    feat: Criando página de Clientes

---
+++ src/app/clientes/page.jsx
@@ -0,0 +1,12 @@
+export default function PaginaClientes() {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>
+          Seja bem-vindo a Página de Clientes do Trabalho de Versionamento de Software!
+          <br />
+        </h1>
+      </main>
+    </>
+  );
+}
