c9b74c27a5e647b6ba1ab25526e02309b8111613
danielvalmeida91 <danielvalmeida91@gmail.com>
2025-07-04 22:05:35 -0300

    feat: Criando página de Produtos

---
+++ src/app/produtos/page.jsx
@@ -0,0 +1,12 @@
+export default function PaginaProdutos() {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>
+          Seja bem-vindo a Página de Produtos do Trabalho de Versionamento de Software!
+          <br />
+        </h1>
+      </main>
+    </>
+  );
+}
