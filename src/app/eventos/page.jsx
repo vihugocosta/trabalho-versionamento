d234b81bac748855f343754d5520db60fc62427f
danielvalmeida91 <danielvalmeida91@gmail.com>
2025-07-04 22:06:54 -0300

    feat: Criando página de Eventos

---
+++ src/app/eventos/page.jsx
@@ -0,0 +1,12 @@
+export default function PaginaEventos() {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>
+          Seja bem-vindo a Página de Eventos do Trabalho de Versionamento de Software!
+          <br />
+        </h1>
+      </main>
+    </>
+  );
+}
