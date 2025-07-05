988038e179d3e9c0ce1895d4b089a44d22050bad
danielvalmeida91 <danielvalmeida91@gmail.com>
2025-07-04 21:42:53 -0300

    Criação da página inicial

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,16 @@
 export default function Home() {
   return (
     <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>
+          Seja bem-vindo ao Trabalho de Versionamento de Software!
+          <br />
+        </h1>
+        <p>
+          Todas as informações necessárias realização do trabalho encontra-se no
+          arquivo <span className="">README.md</span>
+        </p>
+      </main>
     </>
-  )
+  );
 }
